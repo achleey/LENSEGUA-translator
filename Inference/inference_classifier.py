32c6e53c7f9492bdbf78303d99ba67b0b0f6623c
Ashley :) <78820719+achleey@users.noreply.github.com>
2024-11-08 15:50:49 -0600

    Update inference_classifier.py

--- Inference/inference_classifier.py
+++ Inference/inference_classifier.py
@@ -1,30 +1,28 @@
-import pickle
+import pickle  # Para cargar los modelos pre-entrenados almacenados en archivos .pkl
+import cv2  # OpenCV para capturar video, procesar imágenes y mostrar resultados
+import mediapipe as mp  # MediaPipe para la detección de manos, cara y pose en tiempo real
+import numpy as np  # Para trabajar con arrays y operaciones matemáticas (como la distancia y los ángulos)
 
-import cv2
-import mediapipe as mp
-import numpy as np
-
-#Modelo para 1 mano
-model_dict1 = pickle.load(open('./Model1HandP.p', 'rb')) #Cambiar modelos segun el conjunto de datos que se estén utilizando. Ej: Model1HandV.p
+# Cargar los modelos entrenados para diferentes combinaciones de manos y otras partes del cuerpo
+model_dict1 = pickle.load(open('./Model1HandP.p', 'rb'))  # Modelo para 1 mano
 model1 = model_dict1['model']
 
-#Modelo para 2 manos
-model_dict2 = pickle.load(open('./Model2HandsP.p', 'rb'))
+model_dict2 = pickle.load(open('./Model2HandsP.p', 'rb'))  # Modelo para 2 manos
 model2 = model_dict2['model']
 
-#Modelo para mano y cara
-model_dict3 = pickle.load(open('./ModelHandAndFaceP.p', 'rb'))
+model_dict3 = pickle.load(open('./ModelHandAndFaceP.p', 'rb'))  # Modelo para mano y cara
 model3 = model_dict3['model']
 
-#Modelo para mano y cuerpo
-model_dict4 = pickle.load(open('./ModelHandAndBodyP.p', 'rb'))
+model_dict4 = pickle.load(open('./ModelHandAndBodyP.p', 'rb'))  # Modelo para mano y cuerpo
 model4 = model_dict4['model']
 
+# Diccionarios para mapear las etiquetas a las letras
 labels_dict = {0: 'A', 1: 'B', 2:'C', 3:'D', 4: 'E', 5:'K', 6:'L', 7:'M', 8: 'N', 9:'O', 10: 'P', 11: 'R', 12: 'U', 13:'V', 14: 'W', 15: 'Y'}
 labels_dict2 = {0: 'Ñ', 1: 'Q', 2:'X'}
 labels_dict3 = {0: 'G', 1: 'H', 2:'I', 3: 'T'}
 labels_dict4 = {0: 'Z'}
 
+# Inicialización de las soluciones de mediapipe
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.5)
 
@@ -37,8 +35,10 @@ pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3)
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
+# Captura de video
 cap = cv2.VideoCapture(1)
 
+# Función para comprobar si la mano está cerca de la cara
 def is_hand_near_face(hand_landmarks, face_landmarks, threshold=0.03):
     for hand_point in hand_landmarks:
         for face_point in face_landmarks:
@@ -47,7 +47,7 @@ def is_hand_near_face(hand_landmarks, face_landmarks, threshold=0.03):
                 return True
     return False
 
-
+# Función para comprobar si la mano está horizontal
 def is_hand_horizontal(hand_landmarks, angle_threshold=45):
     if not hand_landmarks:
         return False
@@ -64,6 +64,7 @@ def is_hand_horizontal(hand_landmarks, angle_threshold=45):
 
     return np.abs(angle) < angle_threshold or np.abs(angle - 180) < angle_threshold
 
+# Función para verificar si el codo es visible
 def is_elbow_visible(pose_landmarks, hand_landmarks, pose_threshold=0.5, hand_threshold=45):
     if not pose_landmarks or not hand_landmarks:
         return False
@@ -83,12 +84,15 @@ while True:
 
     ret, frame = cap.read()
 
+    # Voltear la imagen para hacer el efecto espejo
     frame = cv2.flip(frame, 1)
 
     H, W, _ = frame.shape
 
+    # Convertir el frame a RGB
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Procesar las manos, cara y pose usando mediapipe
     resultsH = hands.process(frame_rgb)
     resultsF = face.process(frame_rgb)
     resultsP = pose.process(frame_rgb)
@@ -99,6 +103,7 @@ while True:
     hand_data_aux = []
     pose_data_aux = []
 
+    # Si se detectan manos
     if resultsH.multi_hand_landmarks:
         num_hands = len(resultsH.multi_handedness)
         for hand_landmarks in resultsH.multi_hand_landmarks:
@@ -117,6 +122,7 @@ while True:
                 hand_data_aux.append(xh)
                 hand_data_aux.append(yh)
 
+    # Si se detecta una cara
     if resultsF.multi_face_landmarks:
         face_detected = True
         for face_landmarks in resultsF.multi_face_landmarks:
@@ -133,6 +139,7 @@ while True:
                 face_data_aux.append(xf)
                 face_data_aux.append(yf)
 
+    # Si se detecta pose (cuerpo)
     if resultsP.pose_landmarks:
         pose_data = []
         mp_drawing.draw_landmarks(
@@ -155,31 +162,28 @@ while True:
 
     predicted_character = 'No se detecta una combinación válida'
 
-    #if num_hands == 1:
-            #prediction = model1.predict([np.asarray(hand_data_aux)])
-            #predicted_character = labels_dict[int(prediction[0])]
-
-    #elif num_hands == 2:
-            #prediction = model2.predict([np.asarray(hand_data_aux)])
-            #predicted_character = labels_dict2[int(prediction[0])]
-
+    # Detectar una combinación válida de mano y/o cara
     if num_hands >= 1 and face_detected:
         if is_hand_near_face(resultsH.multi_hand_landmarks[0].landmark, resultsF.multi_face_landmarks[0].landmark):
             combination = hand_data_aux + face_data_aux
             prediction = model3.predict([np.asarray(combination)])
             predicted_character = labels_dict3[int(prediction[0])]
         else:
+            # Si solo hay una mano
             if num_hands == 1:
                 prediction = model1.predict([np.asarray(hand_data_aux)])
                 predicted_character = labels_dict[int(prediction[0])]
             else:
+                # Si hay dos manos
                 prediction = model2.predict([np.asarray(hand_data_aux)])
                 predicted_character = labels_dict2[int(prediction[0])]
+    # Si solo se detecta una mano y el codo es visible
     if num_hands == 1 and is_elbow_visible(resultsP.pose_landmarks, resultsH.multi_hand_landmarks[0]):
         combinationByH = hand_data_aux + pose_data_aux
         prediction = model4.predict([np.asarray(combinationByH)])
         predicted_character = labels_dict4[int(prediction[0])]
 
+    # Dibujar un cuadro alrededor de la mano detectada y mostrar el texto de la predicción
     if hand_data_aux:
         x1 = int(min(np.asarray(hand_data_aux)[::2]) * W)
         y1 = int(min(np.asarray(hand_data_aux)[1::2]) * H)
@@ -191,11 +195,15 @@ while True:
                     (0, 0, 0), 5, cv2.LINE_AA)
     else:
         cv2.putText(frame, predicted_character, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.13,
-                    (0, 0, 255), 2, cv2.LINE_AA)
+                    (0, 0, 0), 3, cv2.LINE_AA)
 
-    cv2.imshow('Video', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    # Mostrar la imagen en pantalla
+    cv2.imshow('Frame', frame)
+
+    # Salir si se presiona la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Liberar el video y cerrar las ventanas de OpenCV
 cap.release()
 cv2.destroyAllWindows()
