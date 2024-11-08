0596312df9922956825c3a73fb76a72a63d806ee
Ashley :) <78820719+achleey@users.noreply.github.com>
2024-11-08 15:05:49 -0600

    Update draw_1And2Hands.py

--- Volunteers-Data/1Hand/draw_1And2Hands.py
+++ Volunteers-Data/1Hand/draw_1And2Hands.py
@@ -1,28 +1,39 @@
-import os
-import mediapipe as mp
-import cv2
-import matplotlib.pyplot as plt
+import os  # Para manejar operaciones del sistema de archivos
+import mediapipe as mp  # Librería de visión por computadora para la detección de manos
+import cv2  # Para procesamiento de imágenes
+import matplotlib.pyplot as plt  # Para visualización de imágenes
 
+# Inicialización de mediapipe para la detección de manos
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
+# Configuración de la detección de manos
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+# Directorio y subdirectorio específicos de las imágenes
 DATA_DIR = './1Hand'
 specific_dir = '5'
 
 dir_path = os.path.join(DATA_DIR, specific_dir)
+
+# Verificar si el subdirectorio existe
 if os.path.isdir(dir_path):
+    # Obtener archivos de imagen, ordenados por el número que aparece en el nombre del archivo
     img_files = sorted(
         [f for f in os.listdir(dir_path) if f.endswith(('.png', '.jpg', '.jpeg')) and f.split('.')[0].isdigit()],
-        key=lambda x: int(x.split('.')[0]))
+        key=lambda x: int(x.split('.')[0])
+    )
 
-    for img_path in img_files[:10]:  # Procesar x imágenes
-        img = cv2.imread(os.path.join(dir_path, img_path))
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Procesar las primeras 10 imágenes del directorio
+    for img_path in img_files[:10]:
+        img = cv2.imread(os.path.join(dir_path, img_path))  # Leer la imagen
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convertir la imagen a formato RGB
 
+        # Procesar la imagen para detectar las manos
         results = hands.process(img_rgb)
+
+        # Si se detectan manos, dibujar los landmarks y las conexiones
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
@@ -33,7 +44,8 @@ if os.path.isdir(dir_path):
                     mp_drawing_styles.get_default_hand_connections_style()
                 )
 
+        # Mostrar la imagen con los landmarks dibujados
         plt.figure()
         plt.imshow(img_rgb)
 
-    plt.show()
+    plt.show()  # Mostrar todas las imágenes procesadas
